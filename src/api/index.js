cb6bb32e1736c3562a78bfd54a7cd4033825bfda
gjsimkins <baseball3gary@gmail.com>
2021-08-30 17:58:45 -0700

    Initial commit

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+const URL_SERV = "http://localhost:3004";
+
+export const getPosts = async (prevState, page = 1, order = "asc", limit = "10") => {
+    try {
+        const response = await axios.get(`${URL_SERV}/posts?_page=${page}&_limit=${limit}&_order={order}&_sort=id`);
+
+        return {
+            posts: prevState.posts ? [...prevState.posts, ...response.data] : response.data,
+            page: page,
+            end: response.data.length === 0 ? true : false
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const addNewsletter = async (data) => {
+    try {
+        const findUser = await axios.get(`${URL_SERV}/newsletter?email=${data.email}`);
+        if (!Array.isArray(findUser.data) || !findUser.data.length) {
+            // add user
+            const response = await axios({
+                method: 'POST',
+                url: `${URL_SERV}/newsletter`,
+                data: {
+                    email: data.email
+                }
+            });
+            return {
+                newsletter: 'added',
+                email: response.data
+            }
+        } else {
+            // already on the db
+            return {
+                newsletter: 'failed'
+            }
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getPostsById = async (id) => {
+    try {
+        const response = await axios.get(`${URL_SERV}/posts/${id}`);
+
+        return response.data;
+    } catch (error) {
+        return '404';
+    }
+}
